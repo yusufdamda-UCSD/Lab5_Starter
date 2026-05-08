bb53b70b613cdfc6b401dc37bbe25f04e2419584
Yusuf Damda <ydamda@ucsd.edu>
2026-05-07 20:51:53 -0700

    fix failing sum test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,6 +1,5 @@
 // sum.test.js
 
-test('adds 1 + 2 to equal 4', () => {
-    expect(1 + 2).toBe(4);
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
 });
-// intentionally failing test
